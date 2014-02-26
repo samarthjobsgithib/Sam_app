f2e811ccf3885d60dc2a45f66a5ef7c0f2e9683a
samarthjobsgithib <samarthgithub@gmail.com>
2014-02-26 21:51:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamApp::Application.config.secret_key_base = 'd7fabe589ea2e61e6170ad7446562a0f9f56a146d208d9b836791fc4db285fc82924f9e502089ee1fd6a44d43459fdbd0694637e204cfb7342bb5fbfb897708e'
